2b9e314d5243bb8c0765d311265a005b52b85995
Dustin Koch <dustin.koch@me.com>
2019-05-08 13:35:08 -0600

    Initial Commit

---
+++ TaskManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManager
+//
+//  Created by Dustin Koch on 5/8/19.
+//  Copyright © 2019 Rabbit Hole Fashion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
